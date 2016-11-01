e5e827ed32cda28b1a76d4b399e7d1ce06e4c0f2
System Administrator <root@SYS-Ebisu-0089.local>
2016-11-01 17:57:48 +0900

    change06

--- common-roles/apache/01_apache-server_with-src/templates/apache-configure.sh
+++ common-roles/apache/01_apache-server_with-src/templates/apache-configure.sh
@@ -1,9 +1,6 @@
 ./configure \
---prefix=/usr/local/apache-{{ apache_version }} \
---with-mpm=worker \
---enable-rewrite \
---enable-proxy \
---enable-proxy-ajp \
 --enable-so \
 --enable-ssl \
---enable-headers
+--enable-mods-shared=all \ 
+--with-expat=builtin \ 
+--with-included-apr
