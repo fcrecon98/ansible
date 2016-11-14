c600a46e8a79117050a493580bfdbc65ae5a3fbf
ansible server <choitest@choi.com>
2016-11-14 09:21:12 +0000

    change10

---
+++ system-roles/Tcloud/CBT/percona/02_setup_percona-server/files/delete_slavetable.sql
@@ -0,0 +1,5 @@
+DROP TABLE IF EXISTS `innodb_index_stats`;
+DROP TABLE IF EXISTS `innodb_table_stats`;
+DROP TABLE IF EXISTS `slave_master_info`;
+DROP TABLE IF EXISTS `slave_relay_log_info`;
+DROP TABLE IF EXISTS `slave_worker_info`;
